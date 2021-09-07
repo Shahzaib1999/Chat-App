bba6159b66cbffaf6060f9b4295bf007cc6adc3c
Shahzaib <mohdshahzaib24@gmail.com>
2021-09-08 00:09:07 +0500

    fonts, colors and sizes added

---
+++ src/services/utilities/colors/index.js
@@ -0,0 +1,14 @@
+export const colors = {
+  snow: '#ffffff',
+  black: '#000000',
+  error: 'rgba(200, 0, 0, 0.8)',
+
+  appTextColor1: '#000000',
+  appTextColor2: 'rgba(26,26,26, 1)',
+  appTextColor3: 'rgba(49,49,49, 1)',
+  appTextColor4: '#A1A1A1',
+  appTextColor5: '#FFFF',
+
+  appBgColor1: '#FFFF',
+  appBgColorPrimary: '#1D3932',
+};
